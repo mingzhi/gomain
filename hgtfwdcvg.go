44fcf6ea2846b789646d9df0917b916913dfdb19
Mingzhi Lin <mingzhi9@gmail.com>
2012-09-05 21:49:31 -0400

    select 10 samples and averge

--- hgtfwdcvg.go
+++ hgtfwdcvg.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"github.com/mingzhi/chart/render"
 	"github.com/mingzhi/gomath/random"
+	"github.com/mingzhi/gomath/stat/desc"
 	"github.com/mingzhi/hgt/covs"
 	"github.com/mingzhi/hgt/fwd1"
 	"github.com/vdobler/chart"
@@ -53,11 +54,18 @@ func main() {
 	// do evolution
 	for i := 0; i < ngen; i++ {
 		pop.Evolve()
-		sample := fwd1.Sample(pop.Genomes, sampleSize)
-		dmatrix := fwd1.GenerateDistanceMatrix(sample)
-		cmatrix := covs.NewCMatrix(sampleSize, pop.Length, dmatrix)
-		ks := cmatrix.KS()
-		ksarray = append(ksarray, ks)
+		// select 10 samples and averge
+		mean := desc.NewMean()
+		num := 10
+		for j := 0; j < num; j++ {
+			sample := fwd1.Sample(pop.Genomes, sampleSize)
+			dmatrix := fwd1.GenerateDistanceMatrix(sample)
+			cmatrix := covs.NewCMatrix(sampleSize, pop.Length, dmatrix)
+			ks := cmatrix.KS()
+			mean.Increment(ks)
+		}
+
+		ksarray = append(ksarray, mean.GetResult())
 		ngarray = append(ngarray, float64(pop.NumOfGen))
 	}
 
