13049a5e705e95e559cecf14cbe88d19538b136c
Mingzhi Lin <mingzhi9@gmail.com>
2012-09-05 22:25:18 -0400

    add vard

--- hgtfwdcvg.go
+++ hgtfwdcvg.go
@@ -50,29 +50,50 @@ func init() {
 // the converge of KS equalibrium.
 func main() {
 	ksarray := []float64{} // store ks
+	vdarray := []float64{} // store VarD
 	ngarray := []float64{} // store generation number
 	// do evolution
 	for i := 0; i < ngen; i++ {
 		pop.Evolve()
 		// select 10 samples and averge
 		mean := desc.NewMean()
+		vmean := desc.NewMean()
+		ch := make(chan dResult)
 		num := 10
 		for j := 0; j < num; j++ {
 			sample := fwd1.Sample(pop.Genomes, sampleSize)
-			dmatrix := fwd1.GenerateDistanceMatrix(sample)
-			cmatrix := covs.NewCMatrix(sampleSize, pop.Length, dmatrix)
-			ks := cmatrix.KS()
-			mean.Increment(ks)
+			go calculateD(sample, pop.Length, ch)
+		}
+
+		for j := 0; j < num; j++ {
+			dr := <-ch
+			mean.Increment(dr.ks)
+			vmean.Increment(dr.vard)
 		}
 
 		ksarray = append(ksarray, mean.GetResult())
+		vdarray = append(vdarray, vmean.GetResult())
 		ngarray = append(ngarray, float64(pop.NumOfGen))
 	}
 
 	// draw
-	svger := render.NewSVG(fname, 1, 1, 800, 200)
+	svger := render.NewSVG(fname, 2, 1, 800, 200)
 	pl := chart.ScatterChart{Title: "KS"}
 	pl.AddDataPair("KS", ngarray, ksarray, chart.PlotStyleLines, chart.Style{Symbol: '+', SymbolColor: "#0000ff", LineStyle: chart.SolidLine})
 	svger.Plot(&pl)
+	pl = chart.ScatterChart{Title: "VarD"}
+	pl.AddDataPair("VarD", ngarray, vdarray, chart.PlotStyleLines, chart.Style{Symbol: '+', SymbolColor: "#0000ff", LineStyle: chart.SolidLine})
 	svger.Close()
+}
+
+type dResult struct {
+	ks, vard float64
+}
+
+func calculateD(sample []fwd1.Sequence, length int, ch chan dResult) {
+	dmatrix := fwd1.GenerateDistanceMatrix(sample)
+	cmatrix := covs.NewCMatrix(sampleSize, length, dmatrix)
+	ks, vard := cmatrix.D()
+	dr := dResult{ks: ks, vard: vard}
+	ch <- dr
 }
