19599304e5344cb6965675d6092a62e80a388c17
Mingzhi Lin <mingzhi9@gmail.com>
2012-11-06 18:24:59 -0500

    use channel in coalshpc.go

--- hgtcoals/coalshpc.go
+++ hgtcoals/coalshpc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/mingzhi/gomath/stat/desc"
 	"github.com/mingzhi/hgt/coals"
 	"github.com/mingzhi/hgt/covs"
 	"os"
@@ -21,6 +22,21 @@ var (
 	fragment int     // transfer fragment
 	prefix   string  // prefix
 )
+
+type Moments struct {
+	Mean *desc.Mean
+	Sd   *desc.StandardDeviation
+}
+
+func (m *Moments) Increment(d float64) {
+	m.Mean.Increment(d)
+	m.Sd.Increment(d)
+}
+
+type Results struct {
+	ks, vd                             float64
+	scovs, rcovs, xyPL, xsysPL, smXYPL []float64
+}
 
 func init() {
 	flag.IntVar(&size, "size", 1000000, "population size")
@@ -42,92 +58,123 @@ func init() {
 func main() {
 	ncpu := runtime.NumCPU()
 	runtime.GOMAXPROCS(ncpu)
-	ch := make(chan bool, ncpu)
+	ch := make(chan Results, ncpu)
 
 	t0 := time.Now()
 
 	for i := 0; i < ncpu; i++ {
 		begin := i * repeats / ncpu
 		end := (i + 1) * repeats / ncpu
-		go simusome(begin, end, size, sample, length, mutation, transfer, fragment, prefix, ch)
+		go simusome(begin, end, ch)
 	}
 
-	for i := 0; i < ncpu; i++ {
-		<-ch
-	}
+	analysis(ch)
 
 	t1 := time.Now()
 	fmt.Println(t1.Sub(t0))
 }
 
-func simusome(begin, end, size, sample, length int, mutation, transfer float64, fragment int, prefix string, ch chan bool) {
+func simusome(begin, end int, ch chan Results) {
 	for i := begin; i < end; i++ {
 		w := coals.NewWFPopulation(size, sample, length, mutation, transfer, fragment)
-		simu(w, i, prefix)
+		w.Seed(i)
+		w.Backtrace()
+		seqs := w.Fortrace()
+		diffmatrix := [][]int{}
+		sample := w.SampleSize
+		for i := 0; i < sample; i++ {
+			for j := i + 1; j < sample; j++ {
+				diff := []int{}
+				for k := 0; k < length; k++ {
+					if seqs[i][k] != seqs[j][k] {
+						diff = append(diff, k)
+					}
+				}
+				diffmatrix = append(diffmatrix, diff)
+			}
+		}
+
+		cmatrix := covs.NewCMatrix(len(diffmatrix), length, diffmatrix)
+		ks, vd := cmatrix.D()
+		scovs, rcovs, xyPL, xsysPL, smXYPL := cmatrix.CovCircle(maxl)
+		results := Results{
+			ks:     ks,
+			vd:     vd,
+			scovs:  scovs,
+			rcovs:  rcovs,
+			xyPL:   xyPL,
+			xsysPL: xsysPL,
+			smXYPL: smXYPL,
+		}
+		ch <- results
 	}
-	ch <- true
 }
 
-func simu(w *coals.WFPopulation, idx int, prefix string) {
-	dfile, err := os.Create(fmt.Sprintf("%sd_%d.csv", prefix, idx))
+func analysis(ch chan Results) {
+	dfile, err := os.Create(fmt.Sprintf("%s_d.csv", prefix))
 	if err != nil {
 		panic(err)
 	}
 	defer dfile.Close()
 
-	dfile.WriteString(fmt.Sprintf("#size: %d\n", w.Size))
-	dfile.WriteString(fmt.Sprintf("#length: %d\n", w.GenomeLength))
-	dfile.WriteString(fmt.Sprintf("#fragment: %d\n", w.TransferLength))
-	dfile.WriteString(fmt.Sprintf("#mutation: %g\n", w.MutationRate))
-	dfile.WriteString(fmt.Sprintf("#transfer: %g\n", w.TransferRate))
-	dfile.WriteString(fmt.Sprintf("#sample: %d\n", w.SampleSize))
+	dfile.WriteString(fmt.Sprintf("#size: %d\n", size))
+	dfile.WriteString(fmt.Sprintf("#length: %d\n", length))
+	dfile.WriteString(fmt.Sprintf("#fragment: %d\n", fragment))
+	dfile.WriteString(fmt.Sprintf("#mutation: %g\n", mutation))
+	dfile.WriteString(fmt.Sprintf("#transfer: %g\n", transfer))
+	dfile.WriteString(fmt.Sprintf("#sample: %d\n", sample))
 	dfile.WriteString("#ks, vd\n")
 
-	w.Seed(idx)
-	w.Backtrace()
-	seqs := w.Fortrace()
-	diffmatrix := [][]int{}
-	sample := w.SampleSize
-	for i := 0; i < sample; i++ {
-		for j := i + 1; j < sample; j++ {
-			diff := []int{}
-			for k := 0; k < length; k++ {
-				if seqs[i][k] != seqs[j][k] {
-					diff = append(diff, k)
-				}
+	cfile, err := os.Create(fmt.Sprintf("%s_covs.csv", prefix))
+	if err != nil {
+		panic(err)
+	}
+	defer cfile.Close()
+
+	cfile.WriteString(fmt.Sprintf("#size: %d\n", size))
+	cfile.WriteString(fmt.Sprintf("#length: %d\n", length))
+	cfile.WriteString(fmt.Sprintf("#fragment: %d\n", fragment))
+	cfile.WriteString(fmt.Sprintf("#mutation: %g\n", mutation))
+	cfile.WriteString(fmt.Sprintf("#transfer: %g\n", transfer))
+	cfile.WriteString(fmt.Sprintf("#sample: %d\n", sample))
+	cfile.WriteString("#dist, scov, rcov, xy, xsys, smxy_sd, scov_sd, rcov_sd, xy_sd, xsys_sd, smxy_sd\n")
+
+	momentArr := make([][]Moments, 5)
+	for i := 0; i < len(momentArr); i++ {
+		for j := 0; j < maxl; j++ {
+			moments := Moments{
+				Mean: desc.NewMean(),
+				Sd:   desc.NewStandardDeviationWithBiasCorrection(),
 			}
-			diffmatrix = append(diffmatrix, diff)
+			momentArr[i] = append(momentArr[i], moments)
 		}
 	}
 
-	cmatrix := covs.NewCMatrix(len(diffmatrix), length, diffmatrix)
-	ks, vd := cmatrix.D()
-	dfile.WriteString(fmt.Sprintf("%g,%g\n", ks, vd))
-
-	scovs, rcovs, xyPL, xsysPL, smXYPL := cmatrix.CovCircle(maxl)
-
-	covfile, err := os.Create(fmt.Sprintf("%scovs_%d.csv", prefix, idx))
-	if err != nil {
-		fmt.Println(err)
+	for i := 0; i < repeats; i++ {
+		results := <-ch
+		dfile.WriteString(fmt.Sprintf("%g,%g\n", results.ks, results.vd))
+		for j := 0; j < maxl; j++ {
+			momentArr[0][j].Increment(results.scovs[j])
+			momentArr[1][j].Increment(results.rcovs[j])
+			momentArr[2][j].Increment(results.xyPL[j])
+			momentArr[3][j].Increment(results.xsysPL[j])
+			momentArr[4][j].Increment(results.smXYPL[j])
+		}
 	}
-	defer covfile.Close()
-
-	covfile.WriteString(fmt.Sprintf("#size: %d\n", w.Size))
-	covfile.WriteString(fmt.Sprintf("#length: %d\n", w.GenomeLength))
-	covfile.WriteString(fmt.Sprintf("#fragment: %d\n", w.TransferLength))
-	covfile.WriteString(fmt.Sprintf("#mutation: %g\n", w.MutationRate))
-	covfile.WriteString(fmt.Sprintf("#transfer: %g\n", w.TransferRate))
-	covfile.WriteString(fmt.Sprintf("#sample: %d\n", w.SampleSize))
-	covfile.WriteString("#dist, scov, rcov, xy, xsys, smxy\n")
 
 	for i := 0; i < maxl; i++ {
-		covfile.WriteString(fmt.Sprintf("%d,%g,%g,%g,%g,%g\n",
+		cfile.WriteString(fmt.Sprintf("%d,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g\n",
 			i,
-			scovs[i],
-			rcovs[i],
-			xyPL[i],
-			xsysPL[i],
-			smXYPL[i],
+			momentArr[0][i].Mean.GetResult(),
+			momentArr[1][i].Mean.GetResult(),
+			momentArr[2][i].Mean.GetResult(),
+			momentArr[3][i].Mean.GetResult(),
+			momentArr[4][i].Mean.GetResult(),
+			momentArr[0][i].Sd.GetResult(),
+			momentArr[1][i].Sd.GetResult(),
+			momentArr[2][i].Sd.GetResult(),
+			momentArr[3][i].Sd.GetResult(),
+			momentArr[4][i].Sd.GetResult(),
 		))
 	}
 }
